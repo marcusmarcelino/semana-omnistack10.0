58dede7098cb5269b296ac9aafb7129c9754f497
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-01-14 16:42:21 -0300

    Implementação da filtragem de busca de um Dev próximo

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -10,7 +10,16 @@ module.exports =  {
     const devs = await Dev.find({
       techs: {
         $in: techsArray,
-      }
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        },
+      },
     });
 
     return response.json({ devs });
