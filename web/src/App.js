a50dfe44048a139280ebbb289d2ff4698b9a2d22
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-01-15 17:18:34 -0300

    Inicio da implementação da aplicação Web

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Component:
+/*Função que retorna algum conteudo html*/
+//Estado:
+/**/
+//Propriedade
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
