f0e82c7efa1138271c9794a92e5b86162fd3ebd6
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-01-20 13:49:27 -0300

    Refatoramento de código, correção de funcionalidades avançadas

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.5.16:3333',
-});
+  baseURL: 'http://192.168.5.17:3333',//aqui vai depender do ip que for gerado
+});                                   //quando o script for executado deve-se verificar no terminar o ip correto
 
 export default api;
